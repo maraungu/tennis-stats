0fb536243d13441a0bec9f872c455f109ef32447
maraungu <86411069+maraungu@users.noreply.github.com>
2021-10-12 20:38:11 +0200

    more cli

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import scrape
+from scrape import make_soup
 import click
 
 
 @click.command()
 @click.option('--gender', prompt='Male of female?', help='Male of female tennis players')
-def players(gender):
+@click.option('--birthyear', prompt='Earliest birth year:', help='Only consider tennis players born after that year')
+def players(gender, birthyear):
+    link = ''
     if gender == 'male' or gender == 'Male':
         link = 'https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players'
         print('yay male')
@@ -14,6 +17,8 @@ def players(gender):
             print('yay female')
         else:
             print('Cannot read answer')
+    if birthyear >= '1950':
+        make_soup(link)
 
 
 # Press the green button in the gutter to run the script.
--- scrape.py
+++ scrape.py
@@ -3,18 +3,21 @@ import requests
 import pandas
 from bs4 import BeautifulSoup
 
+
 def get_html(link):
-    #html = requests.get('https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players')
+    # html = requests.get('https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players')
     return requests.get(link)
 
-def ok_to_scrape(html):
+
+def ok_to_scrape(link):
+    html = get_html(link)
     if html.status_code == 200:
         return True
     else:
         return False
 
+
 def make_soup(link):
     html = get_html(link)
-    if ok_to_scrape(html):
+    if ok_to_scrape(link):
         soup = BeautifulSoup(html.text, 'html.parser')
-
