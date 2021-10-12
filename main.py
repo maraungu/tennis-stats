478c1926bad55051eadbee3271d8f8bd91cd67f7
maraungu <86411069+maraungu@users.noreply.github.com>
2021-10-12 17:07:07 +0200

    start cli

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import scrape
+import click
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@click.command()
+@click.option('--gender', prompt='Male of female?', help='Male of female tennis players')
+def players(gender):
+    if gender == 'male' or gender == 'Male':
+        link = 'https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players'
+        print('yay male')
+    else:
+        if gender == 'female' or gender == 'Female':
+            link = 'https://en.wikipedia.org/wiki/List_of_female_tennis_players'
+            print('yay female')
+        else:
+            print('Cannot read answer')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print_hi('PyCharm')
-    print(scrape.ok_to_scrape())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # print(scrape.ok_to_scrape())
+    players()
--- scrape.py
+++ scrape.py
@@ -3,17 +3,18 @@ import requests
 import pandas
 from bs4 import BeautifulSoup
 
-def get_html():
-    html = requests.get('https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players')
-    return html
+def get_html(link):
+    #html = requests.get('https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players')
+    return requests.get(link)
 
-def ok_to_scrape():
-    html = get_html()
+def ok_to_scrape(html):
     if html.status_code == 200:
         return True
     else:
         return False
 
-def make_soup():
-    soup = BeautifulSoup(html.text, 'html.parser')
+def make_soup(link):
+    html = get_html(link)
+    if ok_to_scrape(html):
+        soup = BeautifulSoup(html.text, 'html.parser')
 
