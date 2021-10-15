d2160f268302c64505767db35e23632118d166c9
maraungu <86411069+maraungu@users.noreply.github.com>
2021-10-15 16:25:18 +0200

    start with cmd module

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 import scrape
 from scrape import make_soup
-import argparse
+import cmd, sys
+from aenum import Enum
 
 
-class Players(object):
-    def __init__(self, gender='', birthyear='1950', link='', alive=True, nationality=''):
+class Gender(Enum):
+    female = 1
+    male = 2
+
+
+class Nationality(Enum):
+    any = 1
+    US = 2
+    France = 3
+    Germany = 4
+    Russia = 5
+
+
+class Players:
+    def __init__(self, gender=Gender.female.name, birthyear='1950', link='', alive=True,
+                 nationality=Nationality.any.name):
         self.gender = gender
         self.birthyear = birthyear
         self.link = link
         self.alive = alive
         self.nationality = nationality
+
+
+class TennisPlayersShell(cmd.Cmd):
+    """This is a simple shell for interactive tennis stats"""
+    intro = 'Welcome to the tennis stats shell.  Type help or ? to list commands. \n'
+    prompt = '(tennis)'
+    file = None
+
+    default_player = Players()
+
+    # --------- basic commands ----------
+    def do_exit(self, arg):
+        """Exit the shell"""
+        print('Thank you for using tennis-stats')
+        raise SystemExit()
+
+    def do_showdefaults(self, arg):
+        """Shows the default player parameters"""
+        print("gender: {} \n birth year: {} \n alive: {} \n nationality: {}".format(self.default_player.gender,
+                                                                                    self.default_player.birthyear,
+                                                                                    self.default_player.alive,
+                                                                                    self.default_player.nationality))
+
+    def do_playergender(self, arg):
+        """Choose player gender (male or female): PLAYERGENDER female"""
+        print("hello")
 
     # if gender == 'male' or gender == 'Male':
     #     players.link = 'https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players'
@@ -29,17 +70,4 @@ class Players(object):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("gender", help="pick player gender: male or female")
-    #parser.add_argument("birthyear", help="earliest birth year. default is 1950")
-    #parser.add_argument("alive", help="alive or dead. default doesn't care")
-    #parser.add_argument("nationality", help="default doesn't care")
-    args = parser.parse_args()
-    link = ''
-    if args.gender == 'male':
-        link = 'https://en.wikipedia.org/wiki/List_of_male_singles_tennis_players'
-        print('male')
-    else:
-        if args.gender == 'female':
-            link = 'https://en.wikipedia.org/wiki/List_of_female_tennis_players'
-            print('female')
+    TennisPlayersShell().cmdloop()
